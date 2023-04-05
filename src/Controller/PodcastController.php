2cde4ca366b7ef189e78c818a64d6395424aa80e
Menen <szymonmenen1@gmail.com>
2023-04-05 12:10:20 +0200

    Etape definition podcastController

--- src/Controller/PodcastController.php
+++ src/Controller/PodcastController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PodcastController extends AbstractController
 {
@@ -13,5 +14,27 @@ class PodcastController extends AbstractController
     {
         
         return $this->render('podcast/create.html.twig');
+    }
+
+
+    #[Route('/podcast/confirmation', name: 'podcast_confirmation')]
+    public function podcastConfirmation(Request $req): Response
+    {
+        $songsIds = $req->request->all()['songs'];
+
+        // $podcast = new Podcast();
+        // foreach ($songsIds as $songId){
+        //     // chercher le audio dans la bd
+        //     find($songId)
+        //     // faire add dans le podcast
+        //     $podcast->addSong($song)
+        //     // persist et flush
+
+        // }
+
+        
+
+        dd('entregistrement ok');
+        // return $this->render('podcast/confirmation.html.twig');
     }
 }
